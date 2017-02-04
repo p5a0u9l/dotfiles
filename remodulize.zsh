47c080c858d2c7fce5356396a0ff5dc425fb5f6f
Paul Adams <paul@amalthea.bad>
2017-02-03 20:17:03 -0800

    revamp submods

---
+++ remodulize.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# zsh modules
+git submodule add https://github.com/dracula/zsh.git modules/zsh/dracula-theme
+git submodule add https://github.com/zsh-users/zsh-autosuggestions modules/zsh/zsh-autosuggestions 
+git submodule add https://github.com/robbyrussell/oh-my-zsh.git modules/zsh/oh-my-zsh.git
+
+# vim modules
+git submodule add https://github.com/ctrlpvim/ctrlp.vim modules/vim/ctrlp.vim
+git submodule add https://github.com/tpope/vim-surround modules/vim/vim-surround
+git submodule add https://github.com/tpope/vim-fugitive modules/vim/vim-fugitive
+git submodule add https://github.com/flazz/vim-colorschemes modules/vim/vim-colorschemes
+git submodule add https://github.com/tpope/vim-pathogen modules/vim/vim-pathogen
+git submodule add https://github.com/tpope/vim-commentary modules/vim/vim-commentary
+git submodule add https://github.com/lazywei/vim-matlab modules/vim/vim-matlab
+git submodule add https://github.com/xolox/vim-easytags modules/vim/vim-easytags
+git submodule add https://github.com/xolox/vim-misc modules/vim/vim-misc
+git submodule add https://github.com/Shougo/neocomplcache modules/vim/neocomplcache
+git submodule add https://github.com/Shougo/deoplete.nvim modules/vim/deoplete.nvim
+git submodule add https://github.com/Shougo/neocomplete modules/vim/neocomplete
+git submodule add https://github.com/Shougo/neosnippet modules/vim/neosnippet
+git submodule add https://github.com/Shougo/neosnippet-snippets modules/vim/neosnippet-snippets
+git submodule add https://github.com/zchee/deoplete-jedi modules/vim/deoplete-jedi
+git submodule add https://github.com/vim-airline/vim-airline modules/vim/vim-airline
+git submodule add https://github.com/vim-airline/vim-airline-themes modules/vim/vim-airline-themes
+git submodule add https://github.com/neomake/neomake modules/vim/neomake
