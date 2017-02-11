a92f7420e802e6da1532247af033e8adfa3edc74
Paul Adams <pauladams509@gmail.com>
2017-02-10 22:35:18 -0800

    try out prezto

--- install.zsh
+++ install.zsh
@@ -1,11 +1,6 @@
 #!/bin/zsh
 # a script for installing links to activate dotfiles/modules
 
-DOT_HOME=$HOME/dotfiles
-DOT_DOTS=$DOT_HOME/dots
-DOT_MODS=$DOT_HOME/modules
-XDG=$HOME/.config
-
 # inputs:
 # 1 --> src
 # 2 --> dst
@@ -18,34 +13,33 @@ link_me() {
     ln -s $1 $2
 }
 
-# dot file names should follow convention
 # SUBFOLDERNAME_DOTFILENAME
 link_dots() {
-    link_me $DOT_DOTS/beets_config.yaml $XDG/beets/config.yaml
-    link_me $DOT_DOTS/bpython_config $XDG/bpython/config
-    link_me $DOT_DOTS/cmus_autosave $XDG/cmus_autosave
-    link_me $DOT_DOTS/gitconfig $HOME/.gitconfig
-    link_me $DOT_DOTS/gpg-agent.conf $HOME/.gnupg/gpg
-    link_me $DOT_DOTS/gpg.cong $HOME/.gnupg/gpg
-    link_me $DOT_DOTS/nvim_init.vim $XDG/nvim/init.vim
-    link_me $DOT_DOTS/ipython_config.py $HOME/.ipython_config
-    link_me $DOT_DOTS/pandoc_default.latex $HOME/.pandoc/default.latex
-    link_me $DOT_DOTS/pyrc $HOME/.pyrc
-    link_me $DOT_DOTS/shairport-sync.conf $XDG/shairport-sync/shairport-sync.conf
-    link_me $DOT_DOTS/ssh_config $HOME/.ssh/config
-    link_me $DOT_DOTS/tmux.conf $HOME/.tmux.conf
-    link_me $DOT_DOTS/vifmrc $XDG/vifm/vifmrc
-    link_me $DOT_DOTS/vimrc $HOME/.vimrc
-    link_me $DOT_DOTS/zshrc $HOME/.zshrc
+    link_me $DOTDOTS/beets_config.yaml      $XDG/beets/config.yaml
+    link_me $DOTDOTS/bpython_config         $XDG/bpython/config
+    link_me $DOTDOTS/cmus_autosave          $XDG/cmus_autosave
+    link_me $DOTDOTS/shairport-sync.conf    $XDG/shairport-sync/shairport-sync.conf
+    link_me $DOTDOTS/nvim_init.vim          $XDG/nvim/init.vim
+    link_me $DOTDOTS/vifmrc                 $XDG/vifm/vifmrc
+    link_me $DOTDOTS/gitconfig              $HOME/.gitconfig
+    link_me $DOTDOTS/gpg-agent.conf         $HOME/.gnupg/gpg
+    link_me $DOTDOTS/gpg.conf               $HOME/.gnupg/gpg
+    link_me $DOTDOTS/ipython_config.py      $HOME/.ipython_config
+    link_me $DOTDOTS/pandoc_default.latex   $HOME/.pandoc/default.latex
+    link_me $DOTDOTS/pyrc                   $HOME/.pyrc
+    link_me $DOTDOTS/ssh_config             $HOME/.ssh/config
+    link_me $DOTDOTS/tmux.conf              $HOME/.tmux.conf
+    link_me $DOTDOTS/vimrc                  $HOME/.vimrc
+    link_me $DOTDOTS/zshrc                  $HOME/.zshrc
 }
 
 # modules
 link_modules() {
     # git submodules
-    link_me $DOT_MODS/zsh/oh-my-zsh.git $HOME/.oh-my-zsh.git
-    link_me $DOT_MODS/vim/bundle $XDG/nvim/bundle
-    link_me $DOT_MODS/vim/bundle/vim-pathogen/autoload $XDG/nvim/autoload
-    link_me $DOT_MODS/vim/filetypes.vim $HOME/.config/nvim/filetypes.vim
+    link_me $DOTMODS/zsh/prezto $HOME/.zprezto
+    link_me $DOTMODS/vim/bundle $XDG/nvim/bundle
+    link_me $DOTMODS/vim/bundle/vim-pathogen/autoload $XDG/nvim/autoload
+    link_me $DOTMODS/vim/filetypes.vim $HOME/.config/nvim/filetypes.vim
 }
 
 # main
--- remodulize.zsh
+++ remodulize.zsh
@@ -1,28 +1,28 @@
 #!/bin/zsh
 
 # zsh modules
-git submodule add https://github.com/dracula/zsh.git modules/zsh/dracula-theme
-git submodule add https://github.com/zsh-users/zsh-autosuggestions modules/zsh/zsh-autosuggestions 
-git submodule add https://github.com/robbyrussell/oh-my-zsh.git modules/zsh/oh-my-zsh.git
+git submodule add https://github.com/dracula/zsh.git                modules/zsh/dracula-theme
+git submodule add https://github.com/zsh-users/zsh-autosuggestions  modules/zsh/zsh-autosuggestions
+git submodule add https://github.com/robbyrussell/oh-my-zsh.git     modules/zsh/oh-my-zsh.git
 
 # vim modules
-git submodule add https://github.com/ctrlpvim/ctrlp.vim modules/vim/bundle/ctrlp.vim
-git submodule add https://github.com/tpope/vim-surround modules/vim/bundle/vim-surround
-git submodule add https://github.com/tpope/vim-fugitive modules/vim/bundle/vim-fugitive
-git submodule add https://github.com/flazz/vim-colorschemes modules/vim/bundle/vim-colorschemes
-git submodule add https://github.com/tpope/vim-pathogen modules/vim/bundle/vim-pathogen
-git submodule add https://github.com/tpope/vim-commentary modules/vim/bundle/vim-commentary
-git submodule add https://github.com/lazywei/vim-matlab modules/vim/bundle/vim-matlab
-git submodule add https://github.com/xolox/vim-easytags modules/vim/bundle/vim-easytags
-git submodule add https://github.com/xolox/vim-misc modules/vim/bundle/vim-misc
-git submodule add https://github.com/Shougo/neocomplcache modules/vim/bundle/neocomplcache
-git submodule add https://github.com/Shougo/deoplete.nvim modules/vim/bundle/deoplete.nvim
-git submodule add https://github.com/Shougo/neosnippet modules/vim/bundle/neosnippet
+git submodule add https://github.com/ctrlpvim/ctrlp.vim         modules/vim/bundle/ctrlp.vim
+git submodule add https://github.com/tpope/vim-surround         modules/vim/bundle/vim-surround
+git submodule add https://github.com/tpope/vim-fugitive         modules/vim/bundle/vim-fugitive
+git submodule add https://github.com/flazz/vim-colorschemes     modules/vim/bundle/vim-colorschemes
+git submodule add https://github.com/tpope/vim-pathogen         modules/vim/bundle/vim-pathogen
+git submodule add https://github.com/tpope/vim-commentary       modules/vim/bundle/vim-commentary
+git submodule add https://github.com/lazywei/vim-matlab         modules/vim/bundle/vim-matlab
+git submodule add https://github.com/xolox/vim-easytags         modules/vim/bundle/vim-easytags
+git submodule add https://github.com/xolox/vim-misc             modules/vim/bundle/vim-misc
+git submodule add https://github.com/Shougo/neocomplcache       modules/vim/bundle/neocomplcache
+git submodule add https://github.com/Shougo/deoplete.nvim       modules/vim/bundle/deoplete.nvim
+git submodule add https://github.com/Shougo/neosnippet          modules/vim/bundle/neosnippet
 git submodule add https://github.com/Shougo/neosnippet-snippets modules/vim/bundle/neosnippet-snippets
-git submodule add https://github.com/zchee/deoplete-jedi modules/vim/bundle/deoplete-jedi
-git submodule add https://github.com/vim-airline/vim-airline modules/vim/bundle/vim-airline
+git submodule add https://github.com/zchee/deoplete-jedi        modules/vim/bundle/deoplete-jedi
+git submodule add https://github.com/vim-airline/vim-airline    modules/vim/bundle/vim-airline
 git submodule add https://github.com/vim-airline/vim-airline-themes modules/vim/bundle/vim-airline-themes
-git submodule add https://github.com/neomake/neomake modules/vim/bundle/neomake
+git submodule add https://github.com/neomake/neomake            modules/vim/bundle/neomake
 
 # and demodularize
 # git submodule foreach 'cd ../../../ && git submodule deinit -f '
