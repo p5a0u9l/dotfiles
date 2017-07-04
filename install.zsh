22c0cb59270a0b82cb31d9c17b1c82c652609412
Paul Adams <pauladams509@gmail.com>
2017-07-03 21:29:43 -0700

    fix/update install.zsh

--- install.zsh
+++ install.zsh
@@ -8,16 +8,19 @@ source $HOME/dotfiles/dots/zshenv
 # 2 --> dst
 link() {
     echo "linking $1 --> $2..."
-    ln -sf $1 $2
+    if ! [[ -d `dirname $2` ]]; then
+        mkdir `dirname $2`
+    fi
+    if ! [[ -L $2 ]]; then
+        ln -s $1 $2
+    fi
 }
 
 # SUBFOLDERNAME_DOTFILENAME
 link_dots() {
-    link $DOTDOTS/beets_config.yaml      $XDG/beets/config.yaml
     link $DOTDOTS/bpython_config         $XDG/bpython/config
     link $DOTDOTS/cmus_autosave          $XDG/cmus_autosave
     link $DOTDOTS/shairport-sync.conf    $XDG/shairport-sync/shairport-sync.conf
-    link $DOTDOTS/nvim_init.vim          $XDG/nvim/init.vim
     link $DOTDOTS/vifmrc                 $XDG/vifm/vifmrc
     link $DOTDOTS/gitconfig              $HOME/.gitconfig
     link $DOTDOTS/gitignore              $HOME/.gitignore
@@ -31,20 +34,20 @@ link_dots() {
     link $DOTDOTS/tmux.conf              $HOME/.tmux.conf
     link $DOTDOTS/vimrc                  $HOME/.vimrc
     link $DOTDOTS/zshrc                  $HOME/.zshrc
-    link $DOTDOTS/zshenv                 $HOME/.zshenv
+    link $DOTDOTS/zsh_history            $HOME/.zsh_history
 }
 
 # modules
 link_modules() {
     # zsh
     # ---
-    link $DOTMODS/zsh/oh-my-zsh $HOME/.oh-my-zsh
     link $DOTMODS/tmux/plugins/ $HOME/.tmux/plugins
+
     # vim
     # ---
     link $DOTMODS/vim $VIMHOME
-    link $DOTMODS/vim/bundle/vim-pathogen/autoload $VIMHOME/autoload
-    link $DOTDOTS/filetypes.vim $DOTMODS/vim/filetypes.vim
+    link $DOTMODS/vim/bundle/vim-pathogen/autoload $VIMHOME/
+
     # python
     # ------
     link $DOTMODS/python/pyenv $HOME/.pyenv
